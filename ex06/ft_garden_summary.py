33fdd9bc7145bcc44c0128807e238c3dd949b889
Tatiana <94292144+tilimili13@users.noreply.github.com>
2026-01-26 18:55:58 +0900

    Remove header comments and add status print

--- ex06/ft_garden_summary.py
+++ ex06/ft_garden_summary.py
@@ -1,18 +1,7 @@
-# **************************************************************************** #
-#                                                                              #
-#                                                         :::      ::::::::    #
-#    ft_garden_summary.py                               :+:      :+:    :+:    #
-#                                                     +:+ +:+         +:+      #
-#    By: tnikitin <tnikitin@student.42tokyo.jp>     +#+  +:+       +#+         #
-#                                                 +#+#+#+#+#+   +#+            #
-#    Created: 2026/01/26 17:22:28 by tnikitin          #+#    #+#              #
-#    Updated: 2026/01/26 17:22:28 by tnikitin         ###   ########.fr        #
-#                                                                              #
-# **************************************************************************** #
-
 def ft_garden_summary():
     name = input("Enter garden name: ")
     plants = input("Enter number of plants: ")
     print("Garden:", name)
     print("Plants:", plants)
+
     print("Status: Growing well!")
