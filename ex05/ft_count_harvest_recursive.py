190a577a0d187e68277750121697c34e471db57a
Tatiana <94292144+tilimili13@users.noreply.github.com>
2026-01-26 18:55:40 +0900

    Refactor ft_count_harvest_recursive function

--- ex05/ft_count_harvest_recursive.py
+++ ex05/ft_count_harvest_recursive.py
@@ -1,15 +1,3 @@
-# **************************************************************************** #
-#                                                                              #
-#                                                         :::      ::::::::    #
-#    ft_count_harvest_recursive.py                      :+:      :+:    :+:    #
-#                                                     +:+ +:+         +:+      #
-#    By: tnikitin <tnikitin@student.42tokyo.jp>     +#+  +:+       +#+         #
-#                                                 +#+#+#+#+#+   +#+            #
-#    Created: 2026/01/26 17:08:25 by tnikitin          #+#    #+#              #
-#    Updated: 2026/01/26 17:08:25 by tnikitin         ###   ########.fr        #
-#                                                                              #
-# **************************************************************************** #
-
 def ft_count_harvest_recursive(day=1, days=None):
     if days is None:
         days = int(input("Days until harvest: "))
@@ -17,4 +5,5 @@ def ft_count_harvest_recursive(day=1, days=None):
         print("Harvest time!")
         return
     print("Day", day)
+
     ft_count_harvest_recursive(day + 1, days)
