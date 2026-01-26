56de25cfe5c3a55c6d1046e550847bb7ce4a5679
Tatiana <94292144+tilimili13@users.noreply.github.com>
2026-01-26 18:54:20 +0900

    Remove header comments from ft_plot_area.py

--- ex01/ft_plot_area.py
+++ ex01/ft_plot_area.py
@@ -1,16 +1,5 @@
-# **************************************************************************** #
-#                                                                              #
-#                                                         :::      ::::::::    #
-#    ft_plot_area.py                                    :+:      :+:    :+:    #
-#                                                     +:+ +:+         +:+      #
-#    By: tnikitin <tnikitin@student.42tokyo.jp>     +#+  +:+       +#+         #
-#                                                 +#+#+#+#+#+   +#+            #
-#    Created: 2026/01/26 16:40:52 by tnikitin          #+#    #+#              #
-#    Updated: 2026/01/26 16:40:52 by tnikitin         ###   ########.fr        #
-#                                                                              #
-# **************************************************************************** #
-
 def ft_plot_area():
     length = int(input("Enter length: "))
     width = int(input("Enter width: "))
+
     print("Area: ", length * width)
