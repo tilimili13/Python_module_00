b47854e474f8aed089ff92e209e21a372308b41c
Tatiana <94292144+tilimili13@users.noreply.github.com>
2026-01-26 18:54:38 +0900

    Remove header comments from ft_harvest_total.py

--- ex02/ft_harvest_total.py
+++ ex02/ft_harvest_total.py
@@ -1,17 +1,6 @@
-# **************************************************************************** #
-#                                                                              #
-#                                                         :::      ::::::::    #
-#    ft_harvest_total.py                                :+:      :+:    :+:    #
-#                                                     +:+ +:+         +:+      #
-#    By: tnikitin <tnikitin@student.42tokyo.jp>     +#+  +:+       +#+         #
-#                                                 +#+#+#+#+#+   +#+            #
-#    Created: 2026/01/26 16:45:16 by tnikitin          #+#    #+#              #
-#    Updated: 2026/01/26 16:45:16 by tnikitin         ###   ########.fr        #
-#                                                                              #
-# **************************************************************************** #
-
 def ft_harvest_total():
     day1 = int(input("Day 1 harvest: "))
     day2 = int(input("Day 2 harvest: "))
     day3 = int(input("Day 3 harvest: "))
+
     print("Total harvest: ", day1 + day2 + day3)
