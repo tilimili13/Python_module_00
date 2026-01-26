6e882ec4df23d01c1102c428dfb674012baa0a4b
Tatiana <94292144+tilimili13@users.noreply.github.com>
2026-01-26 18:54:52 +0900

    Remove unnecessary comments and clean up code

--- ex03/ft_plant_age.py
+++ ex03/ft_plant_age.py
@@ -1,18 +1,7 @@
-# **************************************************************************** #
-#                                                                              #
-#                                                         :::      ::::::::    #
-#    ft_plant_age.py                                    :+:      :+:    :+:    #
-#                                                     +:+ +:+         +:+      #
-#    By: tnikitin <tnikitin@student.42tokyo.jp>     +#+  +:+       +#+         #
-#                                                 +#+#+#+#+#+   +#+            #
-#    Created: 2026/01/26 16:49:19 by tnikitin          #+#    #+#              #
-#    Updated: 2026/01/26 16:49:19 by tnikitin         ###   ########.fr        #
-#                                                                              #
-# **************************************************************************** #
-
 def ft_plant_age():
 	age = int(input("Enter the plant's age in days: "))
 	if age < 60:
 		print("Plant needs more time to grow.") 
 	else:
+
 		print("Plant is ready for harvest.")
