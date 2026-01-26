7d49d07bb69ae11fc4372a172f60943f689f66fb
Tatiana <94292144+tilimili13@users.noreply.github.com>
2026-01-26 18:55:27 +0900

    Remove header comments from ft_count_harvest_iterative

--- ex05/ft_count_harvest_iterative.py
+++ ex05/ft_count_harvest_iterative.py
@@ -1,19 +1,8 @@
-# **************************************************************************** #
-#                                                                              #
-#                                                         :::      ::::::::    #
-#    ft_count_harvest_iterative.py                      :+:      :+:    :+:    #
-#                                                     +:+ +:+         +:+      #
-#    By: tnikitin <tnikitin@student.42tokyo.jp>     +#+  +:+       +#+         #
-#                                                 +#+#+#+#+#+   +#+            #
-#    Created: 2026/01/26 17:00:03 by tnikitin          #+#    #+#              #
-#    Updated: 2026/01/26 17:00:03 by tnikitin         ###   ########.fr        #
-#                                                                              #
-# **************************************************************************** #
-
 def ft_count_harvest_iterative():
 	days = int(input("Days until harvest: "))
 	i = 1
 	while i <= days:
 		print("Day", i)
 		i += 1
+
 	print("Harvest time!")
