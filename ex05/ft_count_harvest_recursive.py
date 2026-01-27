12f027b6d66d2b281025dbbe6665aea8963403c0
Tatiana <94292144+tilimili13@users.noreply.github.com>
2026-01-27 19:08:21 +0900

    Refactor harvest counting into separate function

--- ex05/ft_count_harvest_recursive.py
+++ ex05/ft_count_harvest_recursive.py
@@ -1,23 +1,12 @@
-def ft_count_harvest_recursive(day=1, days=None):
-    if days is None:
-        days = int(input("Days until harvest: "))
+def ft_count_harvest_recursive():
+    days = int(input("Days until the harvest: "))
+    _count_days(1, days)
+
+
+def _count_days(day, days):
     if day > days:
         print("Harvest time!")
         return
     print("Day", day)
-
-    ft_count_harvest_recursive(day + 1, days)
-
-#another version
-def ft_count_harvest_recursive(day, days):
-    if day > days:
-        print("Harvest time!")
-        return
-    print("Day", day)
-    ft_count_harvest_recursive(day + 1, days)
-
-
-days = int(input("Days until the harvest: "))
-ft_count_harvest_recursive(1, days)
-
-
+    _count_days(day + 1, days)
+    
