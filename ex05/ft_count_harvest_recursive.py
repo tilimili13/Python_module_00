46c9323478df8a2f1ac4da961d94e2b3d01eeb30
Tatiana <94292144+tilimili13@users.noreply.github.com>
2026-01-27 12:25:39 +0900

    Add alternative version of ft_count_harvest_recursive
    Added an alternative version of the ft_count_harvest_recursive function that does not use a default parameter.

--- ex05/ft_count_harvest_recursive.py
+++ ex05/ft_count_harvest_recursive.py
@@ -7,3 +7,17 @@ def ft_count_harvest_recursive(day=1, days=None):
     print("Day", day)
 
     ft_count_harvest_recursive(day + 1, days)
+
+#another version
+def ft_count_harvest_recursive(day, days):
+    if day > days:
+        print("Harvest time!")
+        return
+    print("Day", day)
+    ft_count_harvest_recursive(day + 1, days)
+
+
+days = int(input("Days until the harvest: "))
+ft_count_harvest_recursive(1, days)
+
+
