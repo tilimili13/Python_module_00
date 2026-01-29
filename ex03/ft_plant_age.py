1479a78e9980e530bf9b98b6b927cecd856e811b
Tatiana <94292144+tilimili13@users.noreply.github.com>
2026-01-29 17:16:52 +0900

    Fix condition for plant harvest readiness check

--- ex03/ft_plant_age.py
+++ ex03/ft_plant_age.py
@@ -1,7 +1,7 @@
 def ft_plant_age():
-	age = int(input("Enter the plant's age in days: "))
-	if age < 60:
-		print("Plant needs more time to grow.") 
-	else:
+    age = int(input("Enter the plant's age in days: "))
+    if age > 60:
+        print("Plant is ready for harvest.")
+    else:
+        print("Plant needs more time to grow.")
 
-		print("Plant is ready for harvest.")
